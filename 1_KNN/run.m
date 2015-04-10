eb5072fe72322f6e479cfeb5af0dc9f86effc36e
Sanghyuk Chun <sanghyuk.chun@gmail.com>
2015-04-10 11:30:34 +0900

    KNN Backup

---
+++ 1_KNN/run.m
@@ -0,0 +1,15 @@
+is_submit = 1;
+fileID = fopen('submission.csv','w');
+fprintf(fileID, 'id,hand\n');
+[ trD, trL, teD, teL ] = getdata( is_submit );
+[ trD, U ] = preprocessing( trD, [] );
+
+teN = size(teD,1);
+for i=1:teN
+    % find class using knn algorithm
+    x = preprocessing(teD(i,:),U);
+    est = knnclassify(x, trD, trL, 6, 'cosine');
+    fprintf(fileID,'%d,%d\n',i,est);
+    disp([num2str(i) '/' num2str(teN)]);
+end
+fclose(fileID);
