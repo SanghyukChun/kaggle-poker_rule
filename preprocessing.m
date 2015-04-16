a7166fca27f036c6aaf69691ee25b87daf96042b
SanghyukChun <sanghyuk.chun@gmail.com>
2015-04-17 00:02:56 +0900

    2nd submission, 0.96908

--- preprocessing.m
+++ preprocessing.m
@@ -1,27 +1,4 @@
 function [ ret ] = preprocessing( data )
-idxperms = nchoosek([2,4,6,8,10],2);
-numperms = size(idxperms,1);
-numdata  = size(data,1);
-ret      = zeros(numperms * 3, numdata);
-ii = 1;
-
-for i=1:numdata
-    x = data(i,:);
-    for j=1:numperms
-        idx = idxperms(j,:);
-        ret(ii,j) = (x(idx(1)) == x(idx(2)));
-    end
-    for j=1:numperms
-        idx = idxperms(j,:);
-        ret(ii,numperms+j) = (x(idx(1)) + 1 == x(idx(2)));
-    end
-    for j=1:numperms
-        idx = idxperms(j,:);
-        ret(ii,2*numperms+j) = (x(idx(1)-1) == x(idx(2)-1));
-    end
-    ii = ii + 1;
-    disp([num2str(i), '/', num2str(numdata)]);
-end
-
-
+ret = data(:,[2,4,6,8,10]);
+ret = sort(ret,2);
 end
--- run.m
+++ run.m
@@ -1,8 +1,18 @@
-is_submit = 0;
+is_submit = 1;
 fileID = fopen('submission.csv','w');
 fprintf(fileID, 'id,hand\n');
 [ trD, trL, teD, teL ] = getdata( is_submit );
 trD = preprocessing( trD );
-% teD = preprocessing( teD );
+teD = preprocessing( teD );
 
+est = knnclassify(teD, trD, trL);
+
+teN = size(teD,1);
+for i=1:teN
+    fprintf(fileID,'%d,%d\n',i,est(i));
+end
 fclose(fileID);
+
+if (~is_submit)
+    disp(num2str(accuracy(est, teL)));
+end
