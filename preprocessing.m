b1ccf47f4c56be29ec841f18cf909585a0f58172
SanghyukChun <sanghyuk.chun@gmail.com>
2015-04-16 23:50:14 +0900

    temp commit

--- preprocessing.m
+++ preprocessing.m
@@ -1,16 +1,27 @@
-function [ ret, U ] = preprocessing( data, U )
-tmp = zeros(size(data,1),52);
-for i=1:5
-    idx = 13 * ( data(:,2*i-1)-1 )+ data(:,2*i);
-    nIdx = size(idx,1);
-    for k=1:nIdx
-        tmp(k,idx(k)) = 1;
-%         disp([num2str(i) ' ' num2str(k) '/' num2str(nIdx)]);
+function [ ret ] = preprocessing( data )
+idxperms = nchoosek([2,4,6,8,10],2);
+numperms = size(idxperms,1);
+numdata  = size(data,1);
+ret      = zeros(numperms * 3, numdata);
+ii = 1;
+
+for i=1:numdata
+    x = data(i,:);
+    for j=1:numperms
+        idx = idxperms(j,:);
+        ret(ii,j) = (x(idx(1)) == x(idx(2)));
     end
+    for j=1:numperms
+        idx = idxperms(j,:);
+        ret(ii,numperms+j) = (x(idx(1)) + 1 == x(idx(2)));
+    end
+    for j=1:numperms
+        idx = idxperms(j,:);
+        ret(ii,2*numperms+j) = (x(idx(1)-1) == x(idx(2)-1));
+    end
+    ii = ii + 1;
+    disp([num2str(i), '/', num2str(numdata)]);
 end
-if isempty(U)
-    Z = bsxfun(@minus,tmp, mean(tmp,1));
-    [U,~,~] = svds(Z', 49);
-end
-ret = tmp * U;
+
+
 end
--- run.m
+++ run.m
@@ -1,15 +1,8 @@
-is_submit = 1;
+is_submit = 0;
 fileID = fopen('submission.csv','w');
 fprintf(fileID, 'id,hand\n');
 [ trD, trL, teD, teL ] = getdata( is_submit );
-[ trD, U ] = preprocessing( trD, [] );
+trD = preprocessing( trD );
+% teD = preprocessing( teD );
 
-teN = size(teD,1);
-for i=1:teN
-    % find class using knn algorithm
-    x = preprocessing(teD(i,:),U);
-    est = knnclassify(x, trD, trL, 6, 'cosine');
-    fprintf(fileID,'%d,%d\n',i,est);
-    disp([num2str(i) '/' num2str(teN)]);
-end
 fclose(fileID);
